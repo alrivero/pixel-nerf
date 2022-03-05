0c7027a40e8f9bcf3adc80b3462f6ce8a989ee9e
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-05 10:48:00 -0500

    Modified __init__ to support PixelNeRF-A

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,11 +1,18 @@
+import sys
 from .models import PixelNeRFNet
 
+_ogpath = sys.path[:]
+sys.path.append('../../Contrib')
+from PixelNeRFNet_A import PixelNeRFNet_A
+sys.path = _ogpath
 
 def make_model(conf, *args, **kwargs):
     """ Placeholder to allow more model types """
     model_type = conf.get_string("type", "pixelnerf")  # single
     if model_type == "pixelnerf":
         net = PixelNeRFNet(conf, *args, **kwargs)
+    else if model_type == "pixelnerf-a":
+        net = PixelNeRFNet_A(conf, *args, **kwargs)
     else:
         raise NotImplementedError("Unsupported model type", model_type)
     return net
