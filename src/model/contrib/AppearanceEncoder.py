b303e03d1a419a2542a67393ded1e18c2024c026
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-07 14:24:47 -0500

    Attempted invalid arguments fix

--- src/model/contrib/AppearanceEncoder.py
+++ src/model/contrib/AppearanceEncoder.py
@@ -4,10 +4,10 @@ from .unet_tile_se_norm import StyleEncoder
 class AppearanceEncoder(StyleEncoder):
     # Identical to the style encoder of SSHarmonization, but loaded from config file
     def __init__(self, conf):
-        n_downsample = conf.get_float("n_downsample", 4)
-        input_dim = conf.get_float("input_dim", 3)
-        dim = conf.get_float("dim", 512)
-        style_dim = conf.get_float("style_dim", 512)
+        n_downsample = conf.get_int("n_downsample", 4)
+        input_dim = conf.get_int("input_dim", 3)
+        dim = conf.get_int("dim", 512)
+        style_dim = conf.get_int("style_dim", 512)
         norm = conf.get_string("norm", "none")
         activ = conf.get_string("activ", "relu")
         pad_type = conf.get_string("pad_type", "reflect")
