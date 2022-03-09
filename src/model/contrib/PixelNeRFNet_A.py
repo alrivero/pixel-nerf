3365dfce6448a193957ad9484a46ff7f616603f5
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-09 15:28:55 -0500

    Appearance encoder addition start

--- src/model/contrib/PixelNeRFNet_A.py
+++ src/model/contrib/PixelNeRFNet_A.py
@@ -43,3 +43,131 @@ class PixelNeRFNet_A(PixelNeRFNet):
                 ).format(model_path)
             )
         return self
+    
+    def forward(self, xyz, coarse=True, viewdirs=None, far=False):
+        """
+        Predict (r, g, b, sigma) at world space points xyz.
+        Please call encode first!
+        :param xyz (SB, B, 3)
+        SB is batch of objects
+        B is batch of points (in rays)
+        NS is number of input views
+        :return (SB, B, 4) r g b sigma
+        """
+        with profiler.record_function("model_inference"):
+            SB, B, _ = xyz.shape
+            NS = self.num_views_per_obj
+
+            # Transform query points into the camera spaces of the input views
+            xyz = repeat_interleave(xyz, NS)  # (SB*NS, B, 3)
+            xyz_rot = torch.matmul(self.poses[:, None, :3, :3], xyz.unsqueeze(-1))[
+                ..., 0
+            ]
+            xyz = xyz_rot + self.poses[:, None, :3, 3]
+
+            if self.d_in > 0:
+                # * Encode the xyz coordinates
+                if self.use_xyz:
+                    if self.normalize_z:
+                        z_feature = xyz_rot.reshape(-1, 3)  # (SB*B, 3)
+                    else:
+                        z_feature = xyz.reshape(-1, 3)  # (SB*B, 3)
+                else:
+                    if self.normalize_z:
+                        z_feature = -xyz_rot[..., 2].reshape(-1, 1)  # (SB*B, 1)
+                    else:
+                        z_feature = -xyz[..., 2].reshape(-1, 1)  # (SB*B, 1)
+
+                if self.use_code and not self.use_code_viewdirs:
+                    # Positional encoding (no viewdirs)
+                    z_feature = self.code(z_feature)
+
+                if self.use_viewdirs:
+                    # * Encode the view directions
+                    assert viewdirs is not None
+                    # Viewdirs to input view space
+                    viewdirs = viewdirs.reshape(SB, B, 3, 1)
+                    viewdirs = repeat_interleave(viewdirs, NS)  # (SB*NS, B, 3, 1)
+                    viewdirs = torch.matmul(
+                        self.poses[:, None, :3, :3], viewdirs
+                    )  # (SB*NS, B, 3, 1)
+                    viewdirs = viewdirs.reshape(-1, 3)  # (SB*B, 3)
+                    z_feature = torch.cat(
+                        (z_feature, viewdirs), dim=1
+                    )  # (SB*B, 4 or 6)
+
+                if self.use_code and self.use_code_viewdirs:
+                    # Positional encoding (with viewdirs)
+                    z_feature = self.code(z_feature)
+
+                mlp_input = z_feature
+
+            if self.use_encoder:
+                # Grab encoder's latent code.
+                uv = -xyz[:, :, :2] / xyz[:, :, 2:]  # (SB, B, 2)
+                uv *= repeat_interleave(
+                    self.focal.unsqueeze(1), NS if self.focal.shape[0] > 1 else 1
+                )
+                uv += repeat_interleave(
+                    self.c.unsqueeze(1), NS if self.c.shape[0] > 1 else 1
+                )  # (SB*NS, B, 2)
+                latent = self.encoder.index(
+                    uv, None, self.image_shape
+                )  # (SB * NS, latent, B)
+
+                if self.stop_encoder_grad:
+                    latent = latent.detach()
+                latent = latent.transpose(1, 2).reshape(
+                    -1, self.latent_size
+                )  # (SB * NS * B, latent)
+
+                if self.d_in == 0:
+                    # z_feature not needed
+                    mlp_input = latent
+                else:
+                    mlp_input = torch.cat((latent, z_feature), dim=-1)
+
+            if self.use_global_encoder:
+                # Concat global latent code if enabled
+                global_latent = self.global_encoder.latent
+                assert mlp_input.shape[0] % global_latent.shape[0] == 0
+                num_repeats = mlp_input.shape[0] // global_latent.shape[0]
+                global_latent = repeat_interleave(global_latent, num_repeats)
+                mlp_input = torch.cat((global_latent, mlp_input), dim=-1)
+            
+            if self.use_app_encoder:
+                # Do something here
+                if self.stop_app_encoder_grad:
+                    pass
+                
+
+            # Camera frustum culling stuff, currently disabled
+            combine_index = None
+            dim_size = None
+
+            # Run main NeRF network
+            if coarse or self.mlp_fine is None:
+                mlp_output = self.mlp_coarse(
+                    mlp_input,
+                    combine_inner_dims=(self.num_views_per_obj, B),
+                    combine_index=combine_index,
+                    dim_size=dim_size,
+                )
+            else:
+                mlp_output = self.mlp_fine(
+                    mlp_input,
+                    combine_inner_dims=(self.num_views_per_obj, B),
+                    combine_index=combine_index,
+                    dim_size=dim_size,
+                )
+
+            # Interpret the output
+            mlp_output = mlp_output.reshape(-1, B, self.d_out)
+
+            rgb = mlp_output[..., :3]
+            sigma = mlp_output[..., 3:4]
+
+            output_list = [torch.sigmoid(rgb), torch.relu(sigma)]
+            output = torch.cat(output_list, dim=-1)
+            output = output.reshape(SB, B, -1)
+        return output
