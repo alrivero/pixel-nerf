3dc74d0bfc411aa7fca138faf2d5087a58134810
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-12 19:48:49 -0500

    Fixed incorrect directory path for AppearanceDataset

--- contrib/data/AppearanceDataset.py
+++ contrib/data/AppearanceDataset.py
@@ -78,7 +78,7 @@ class AppearanceDataset(torch.utils.data.Dataset):
 
         rgb_paths = [
             x
-            for x in glob.glob(os.path.join(root_dir, "image", "*"))
+            for x in glob.glob(os.path.join(root_dir, "images", "dslr_images_undistorted", "*"))
             if (x.endswith(".jpg") or x.endswith(".png"))
         ]
         rgb_paths = sorted(rgb_paths)
