eb2620dcf5cb2b3f70e04862cdf1d0603cd97cc9
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-14 11:40:33 -0400

    More printing for testing 11

--- src/model/PixelNeRFNet_A.py
+++ src/model/PixelNeRFNet_A.py
@@ -63,8 +63,6 @@ class PixelNeRFNet_A(PixelNeRFNet):
 
             # Transform query points into the camera spaces of the input views
             xyz = repeat_interleave(xyz, NS)  # (SB*NS, B, 3)
-            print(xyz[0][0], xyz[4][0])
-            print(xyz[0][0], xyz[1][0])
             xyz_rot = torch.matmul(self.poses[:, None, :3, :3], xyz.unsqueeze(-1))[
                 ..., 0
             ]
