a35ba90ff96a6e0b731f86d07f6f57f8b8a684df
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-15 11:12:05 -0400

    Fixed ind bug in cal_losses

--- contrib/train_app.py
+++ contrib/train_app.py
@@ -283,12 +283,12 @@ class PixelNeRF_ATrainer(trainlib.Trainer):
 
         # Render out the scene normally using an input view as our encoding source
         rand_inview_ind = randint(0, len(src_images[0]) - 1)
-        app_images = src_images[0][rand_inview_ind].unsqueeze(0)
+        inview_app_imgs = src_images[0][rand_inview_ind].unsqueeze(0)
         for i in range(1, args.batch_size):
             rand_inview_ind = randint(0, len(src_images[i]) - 1)
             new_app_tensor = src_images[i][rand_inview_ind].unsqueeze(0)
+            inview_app_imgs = torch.cat([inview_app_imgs, new_app_tensor], 0)
 
-        inview_app_imgs = torch.cat([app_images, new_app_tensor], 0)
         reg_render_dict = self.batch_pass(inview_app_imgs, all_rays)
 
         # Render our scene using the appearance image we're trying to harmonize with
