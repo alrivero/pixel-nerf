8f7ae3aa5ec7a37fd37c80afc5da365219ec4a4e
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-03-25 14:51:27 -0400

    Used expand to match dims

--- src/model/resnetfc.py
+++ src/model/resnetfc.py
@@ -266,7 +266,9 @@ class ResnetFC_App(ResnetFC):
                     x = util.combine_interleaved(
                         x, combine_inner_dims, self.combine_type
                     )
-                    x = torch.cat((app_enc, x), dim=-1)
+                    B, D = app_enc.shape
+                    _, C, _ = x.shape
+                    x = torch.cat((app_enc.expand(B, C, D), x), dim=-1)
                     x = self.app_block(x)
 
                 if self.d_latent > 0 and blkid < self.combine_layer:
