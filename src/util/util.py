wrong dim again

src/util/util.py
@@ -628,7 +628,7 @@ def unit_sphere_intersection(rays):
     cam_pos_dist = torch.norm(cam_pos, p=2, dim=1)
 
     # Since our sphere center is at 0, 0, 0, calculations simplify
-    cam_pos_proj_len = (cam_pos * cam_dir).sum(dim=0)
+    cam_pos_proj_len = (cam_pos * cam_dir).sum(dim=1)
     dist_proj_cent = torch.sqrt((cam_pos_dist ** 2) - (cam_pos_proj_len ** 2))
     dist_intersect = torch.sqrt(1 - (dist_proj_cent ** 2))
 