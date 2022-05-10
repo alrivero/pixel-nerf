5fe0c6c049ea1874ad9839474c9b4db0b6422072
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-10 19:08:39 -0400

    Using preset radius

--- eval/encode_video_path.py
+++ eval/encode_video_path.py
@@ -183,7 +183,7 @@ render_rays = util.gen_rays(
     c=c * args.scale if c is not None else None,
 ).to(device=device)
 # (NV, H, W, 8)
-bounding_radius = util.bounding_sphere_radius(render_rays).unsqueeze(0)
+bounding_radius = torch.tensor(radius)
 render_rays = render_rays.reshape(1, -1, 8)
 render_patches = util.sample_spherical_enc_patches(render_rays, bounding_radius, app_imgs, 223)
 
