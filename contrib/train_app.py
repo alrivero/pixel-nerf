7daea32ec52bd1f5b2528a66e22829f30dc362c9
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-10 21:22:42 -0400

    indexing

--- contrib/train_app.py
+++ contrib/train_app.py
@@ -212,7 +212,7 @@ class PixelNeRF_ATrainer(trainlib.Trainer):
         self.app_enc_on = not args.app_enc_off
         self.calc_losses = self.calc_losses_app if self.app_enc_on else self.calc_losses_no_app
 
-        self.views = torch.arange(0, NV, step=(NV // int(args.nviews)))
+        self.views = torch.arange(0, NV - 1, step=(NV // int(args.nviews)))
         self.views = self.views.unsqueeze(0).expand(SB, -1).to(device=device)
 
         # If we are, that means we're using a background and patch loss
