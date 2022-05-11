6796c7b18a2b2198d8915a830f7aa39824bcb197
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-10 20:54:21 -0400

    indexing indexing and indexing

--- contrib/train_app.py
+++ contrib/train_app.py
@@ -386,10 +386,10 @@ class PixelNeRF_ATrainer(trainlib.Trainer):
         all_rays = torch.stack(all_rays)  # (SB, ray_batch_size, 8)
         all_radii = torch.stack(all_radii)
 
-        image_ord = torch.randint(0, int(args.nviews), (int(args.nviews), 1)).to(device=device)
+        image_ord = torch.randint(0, int(args.nviews), (SB, 1)).to(device=device)
         all_rays = util.batched_index_select_nd(all_rays, image_ord)
         all_rays = all_rays.permute(0, 1, 3, 4, 2).reshape(SB, -1, 8)
-        all_rgb_gt = util.batched_index_select_nd(all_rgb_gt, self.views).reshape(SB, -1, 3)
+        all_rgb_gt = util.batched_index_select_nd(all_rgb_gt, image_ord).reshape(SB, -1, 3)
 
         all_poses = all_images = None
 
