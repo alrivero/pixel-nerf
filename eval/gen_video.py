e0b0670ce2977763b96ecd29d8aabc6f9f7b7ccc
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-16 11:53:42 -0400

    new args

--- eval/gen_video.py
+++ eval/gen_video.py
@@ -276,7 +276,7 @@ with torch.no_grad():
         B, _ = rays.shape
 
         # Some pixels might be really close together and use the same encoding
-        uv_env = util.sample_spherical_uv(rays[None], bounding_radius, app_imgs).reshape(-1, 2)
+        uv_env = util.sample_spherical_uv(rays[None], app_imgs).reshape(-1, 2)
         unique_uv, inv_map = uv_env.unique(dim=0, return_inverse=True)
         unq_u = unique_uv[:, 0].reshape(1, -1, 1)
         unq_v = unique_uv[:, 1].reshape(1, -1, 1)
