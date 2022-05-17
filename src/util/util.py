7ce85e93c36f2bc8e314e931110551c35f9647be
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-17 15:42:28 -0400

    invert y

--- src/util/util.py
+++ src/util/util.py
@@ -619,7 +619,7 @@ def rays_blinn_newell_uv(intersections, radii, app_imgs, patch_size):
 
     azimuth = (torch.atan2(z, x) + (2.0 * pi)) % (2.0 * pi)
     u = (W * (azimuth / (2 * pi))).long()
-    v = (H * (torch.asin(-y / radii) + (pi / 2)) / pi).long()   # Negative y since top-left is 0, 0
+    v = (H * (torch.asin(y / radii) + (pi / 2)) / pi).long()   # Negative y since top-left is 0, 0
 
     return u, v
 
@@ -633,7 +633,7 @@ def longitude_lattitude_norm(intersections, radii, app_imgs):
     radii = radii.expand(SB, B).unsqueeze(-1)
 
     long = torch.atan2(z, x) / pi
-    lat = torch.asin(-y / radii) / (pi / 2)
+    lat = torch.asin(y / radii) / (pi / 2)
 
     return torch.cat((long, lat), dim=-1)
 
