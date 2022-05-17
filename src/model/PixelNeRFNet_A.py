48e48a5690dc8f8ed28079542aa15c34f0677141
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-16 20:37:20 -0400

    reshape dim

--- src/model/PixelNeRFNet_A.py
+++ src/model/PixelNeRFNet_A.py
@@ -254,7 +254,7 @@ class PixelNeRFNet_A(torch.nn.Module):
             # Apply positional encoding to rgb_enc
             if app_pass:
                 long_lat = rgb_enc[:, :, -2:].reshape(-1, 2)
-                long_lat_enc = self.long_lat_code(long_lat).reshape(SB, B, 2)
+                long_lat_enc = self.long_lat_code(long_lat).reshape(SB, B, -1)
                 rgb_enc = torch.cat((rgb_enc[:, :, :-2], long_lat_enc), dim=-1)
 
             # Run main NeRF network
