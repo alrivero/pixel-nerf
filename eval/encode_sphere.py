f540c6c91ffc43b5aafb501161da55b90fe7fafb
Alfredo Rivero <alfredo.rivero@outlook.com>
2022-05-29 23:41:19 -0400

    Added code to precompute patches

---
+++ eval/encode_sphere.py
@@ -0,0 +1,85 @@
+import sys
+import os
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+)
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
+import torch
+import util
+import tqdm
+from data.AppearanceDataset import AppearanceDataset
+from contrib.model.unet_tile_se_norm import StyleEncoder
+
+
+def extra_args(parser):
+    parser.add_argument(
+        "--radius",
+        type=float,
+        default=2.0,
+        help="Distance of camera from origin, default is average of z_far, z_near of dataset (only for non-DTU)",
+    )
+    parser.add_argument(
+        "--appdir", "-DA", type=str, default=None, help="Appearance Dataset directory"
+    )
+    parser.add_argument(
+        "--app_set_ind", "-IA", type=int, default=0, help="Index of image to be used for appearance harmonization"
+    )
+    parser.add_argument(
+        "--app_ind", "-IM", type=int, default=0, help="Index of image to be used for appearance harmonization"
+    )
+    parser.add_argument(
+        "--refencdir", "-DRE", type=str, default=None, help="Reference encoder directory (used for loss)"
+    )
+    parser.add_argument(
+        "--save_dir", "-SD", type=str, default=None, help="Directory to save encodings"
+    )
+    parser.add_argument(
+        "--sphere_subdiv",
+        "-S",
+        type=int,
+        default=200,
+        help="Level of subdivision used for sphere points",
+    )
+    parser.add_argument(
+        "--batch_size",
+        "-B",
+        type=int,
+        default=200,
+        help="# of patches to encode per iteration",
+    )
+    return parser
+
+
+args, conf = util.args.parse_args(extra_args)
+args.resume = True
+
+device = util.get_cuda(args.gpu_id[0])
+
+# Reference encoder used across network
+ref_encoder = StyleEncoder(4, 3, 32, 512, norm="BN", activ="relu", pad_type='reflect').to(device=device)
+ref_encoder.load_state_dict(torch.load(args.refencdir))
+
+print("Encoding Patches...")
+
+dset_app = AppearanceDataset(args.appdir, "train", image_size=(2048, 4096)) # SET IMAGE SIZE
+app_imgs = dset_app[args.app_set_ind][args.app_ind].unsqueeze(0).to(device=device)
+
+sphere_verts = util.uv_sphere(args.radius, args.sphere_subdiv).to(device=device)
+
+radius = torch.tensor(args.radius).unsqueeze(-1).to(device=device)
+
+with torch.no_grad():
+    all_encs = []
+    for rays in tqdm.tqdm(
+        torch.split(sphere_verts, args.batch_size, dim=0)
+    ):
+        patches, _ = util.sample_spherical_patch_rays(rays, sphere_verts, radius, app_imgs, 223, args.sphere_subdiv)
+        batch_encs = ref_encoder(patches)
+        all_encs.append(batch_encs.to(device="cpu"))
+    all_encs = torch.cat(all_encs)
+
+    torch.save(all_encs, args.save_dir)
